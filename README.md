09d13eea2ac9072cdec87671898889547860fb8a
jkulds <w84my5@yandex.ru>
2023-08-20 19:40:12 +0300

    Sixth step

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 [![Actions Status](https://github.com/jkulds/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/jkulds/python-project-49/actions)
 <a href="https://codeclimate.com/github/jkulds/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/d3dd697c8bf3a0ac2e78/maintainability" /></a>
 
-https://asciinema.org/a/GXtTfdFCwYElScZCe842l6afl
+Brain-even game - https://asciinema.org/a/GXtTfdFCwYElScZCe842l6afl <br/>
+Brain-calc game - https://asciinema.org/a/MfyQuSqWadUN3b6MMUmY5jc3s <br/>
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,6 @@
 import prompt
-from random import randint
+from random import randint, choice
+
 
 def welcome_user() -> str:
     user = prompt.string("May I have your name? ")
@@ -12,18 +13,74 @@ def even_game(user_name: str) -> None:
     count = 3
     i = 0
     while i < count:
-        number = randint(1, 100)
-        print("Question:", number)
-        answer = prompt.string("Your answer: ")
-        correct = 'yes' if number % 2 == 0 else 'no'
+        result = print_even_game_question_and_get_result()
 
-        if answer == correct:
-            print("Correct!")
+        answer = prompt.string("Your answer: ")
+
+        if is_answer_correct(answer, result, user_name):
             i += 1
         else:
-            print(f"'{answer}' is wrong answer ;(."
-                  f" Correct answer was '{correct}'.")
-            print(f"Let's try again {user_name}")
             i = 0
 
+    print_user_congratulations(user_name)
+
+
+def print_even_game_question_and_get_result():
+    number = randint(1, 100)
+    print("Question:", number)
+    result = 'yes' if number % 2 == 0 else 'no'
+    return result
+
+
+def calc_game(user_name: str) -> None:
+    print('What is the result of the expression?')
+    count = 3
+    i = 0
+    while i < count:
+        result = print_calc_game_question_and_get_result()
+
+        answer = prompt.integer("Your answer: ")
+
+        if is_answer_correct(answer, result, user_name):
+            i += 1
+        else:
+            i = 0
+
+    print_user_congratulations(user_name)
+
+
+def print_user_congratulations(user_name):
     print(f"Congratulations, {user_name}!")
+
+
+def print_calc_game_question_and_get_result():
+    signs = ['+', '-', '*']
+    sign = choice(signs)
+    min_num = 3
+    max_num = 10
+    a = randint(min_num, max_num)
+    b = randint(min_num, max_num)
+    result = 0
+    match sign:
+        case '+':
+            result = a + b
+        case '-':
+            result = a - b
+        case '*':
+            result = a * b
+
+    print("Question:", a, sign, b)
+
+    return result
+
+
+def is_answer_correct(answer, result, user_name):
+    if answer == result:
+        print("Correct!")
+        return True
+
+    print(f"'{answer}' is wrong answer ;(."
+          f" Correct answer was '{result}'.")
+    print(f"Let's try again {user_name}")
+
+    return False
